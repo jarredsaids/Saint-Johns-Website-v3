ae975caf1552c551cef61e1c6e2a05dd77414b86
David M Turner <jarredsaids@gmail.com>
2020-07-29 14:02:38 -0400

    Adding Galleria
    - added galleria 1.6.1 photo galleries
    - added contollers for all pages
    - added photos for galleries

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,14 +11,34 @@ class PagesController extends Controller
         $title = 'Home';
         return view('pages.index')->with('title', $title);
     }
+
     public function events()
     {
         $title = 'Events';
         return view('pages.events')->with('title', $title);
     }
+
     public function gallery()
     {
         $title = 'Gallery';
         return view('pages.gallery')->with('title', $title);
+
+    }
+
+    public function location()
+    {
+        $title = 'Location';
+        return view('pages.location')->with('title', $title);
+    }
+
+    public function people()
+    {
+        $title = 'Our People';
+        return view('pages.people')->with('title', $title);
+    }
+    public function visit()
+    {
+        $title = 'Visit Us';
+        return view('pages.visit')->with('title', $title);
     }
 }
